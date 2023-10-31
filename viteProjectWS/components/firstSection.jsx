18303882aebf035f47cf32f32537b68f1d4914cb
Su-mia <soumia.bouaouina@ensia.edu.dz>
2023-10-31 23:20:15 +0100

    Initial commit

---
+++ viteProjectWS/components/firstSection.jsx
@@ -0,0 +1,43 @@
+// rafce
+import React from 'react'
+
+const divStyle = {
+    color: 'white',
+    height: '100vh',  
+    width: '100vw',
+    margin : '50px'
+  };
+
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+function getGreeting(user) {
+    if (user) {
+     return <h1>Hello, {formatName(user)}!</h1>;
+   }
+   return <h1>Hello, Stranger.</h1>;
+  }
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+  
+const firstSection = () => {
+
+  return (
+    <div
+    style={divStyle}
+    >
+    // try here
+    
+    </div>
+  )
+}
+
+export default firstSection
+
---
+++ viteProjectWS/src/App.jsx
@@ -0,0 +1,16 @@
+import FirstSection from '../components/firstSection'
+import SecondSection from '../components/secondSection'
+import ThirdSection from '../components/thirdSection'
+
+function App() {
+
+  return (
+    <>
+    <FirstSection/>
+    <SecondSection/>
+    <ThirdSection/>
+    </>
+  )
+}
+
+export default App
